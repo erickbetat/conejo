408ce5a313a753e319e74e6abe5dedc8802e1c02
Erick Betancourt <ebetancourt@treblum.com>
2026-07-06 18:51:17 -0600

    ajustado archivo de factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,12 +24,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $password = Hash::make('password');
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'password' => $password,
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'deleted_at' => null,
         ];
     }
 
