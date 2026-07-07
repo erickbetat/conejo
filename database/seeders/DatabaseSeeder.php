56d45a6ad9256c22c90de15fc145a4c2d023eab7
Erick Betancourt <ebetancourt@treblum.com>
2026-07-06 19:11:35 -0600

    agregado el seeder a database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
     }
 }
